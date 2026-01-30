1ada9103d4ba6709e1b9e09df1b780a92bb5b845
LTSteve <steth1000@gmail.com>
2026-01-30 15:33:00 -0500

    player gains gold per second if units are at top of screen

---
+++ scripts/managers/ransack_manager.gd
@@ -0,0 +1,13 @@
+class_name RansackManager
+extends Node
+
+func _on_timer_timeout() -> void:
+    var count := 0
+    for minion: MinionState in GameState.current.minions:
+        if minion.position.y == 0 && !minion.enemy: # player minion at the end
+            VfxManager.spawn_vfx(GridHelper.grid_position_to_world_position(minion.position),\
+            VfxManager.VFX_FLOATING_NUMBER, 1)
+            count += 1
+            
+    GameState.current.resource_state.player_gold += count
+    GameState.current.resource_state.updated.emit()
