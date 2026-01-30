b13159c3a09c404b38954c2ad3a786f1663c4612
LTSteve <steth1000@gmail.com>
2026-01-30 09:28:52 -0500

    make clicking feel better

--- scripts/behaviours/mouse_click_indicator.gd
+++ scripts/behaviours/mouse_click_indicator.gd
@@ -9,21 +9,35 @@ const CLICK_PERCENT_OFFSET := 0.15
 
 var points : Array[Vector2]
 var click_time := 0.0
-var can_click := true
+var is_fully_charged := false
+var point_cast := PhysicsPointQueryParameters2D.new()
 
 func _ready() -> void:
     for pt: Vector2 in line.points:
         points.append(pt)
     line.clear_points()
+    
+    point_cast.collide_with_areas = true
+    point_cast.collide_with_bodies = false
 
 func _process(delta: float) -> void:
     global_position = get_global_mouse_position()
     
-    if !can_click && !Input.is_action_pressed("click"):
-        can_click = true
+    # On release: fire if fully charged, then reset
+    if Input.is_action_just_released("click"):
+        if is_fully_charged:
+            var space_state = get_world_2d().direct_space_state
+            point_cast.position = global_position
+            var result = space_state.intersect_point(point_cast, 1)
+            if !result.is_empty():
+                var minion := (result[0].collider.get_parent() as Minion).state
+                PlayerClickHelper.on_player_click(minion.position)
+        click_time = 0.0
+        visible = false
+        is_fully_charged = false
         return
     
-    if !can_click || !Input.is_action_pressed("click"):
+    if !Input.is_action_pressed("click"):
         click_time = 0.0
         visible = false
         return
@@ -32,14 +46,13 @@ func _process(delta: float) -> void:
     click_time += delta
     
     var actual_click_percent := click_time / FULL_CLICK_TIME
+    if actual_click_percent >= 1.0:
+        is_fully_charged = true
+        actual_click_percent = 1.0  # Hold at full, don't keep counting
+    
+    line.default_color = ColorConfig.PALE_PURPLE if is_fully_charged else ColorConfig.PURPLE
     var click_percent := clampf(actual_click_percent + CLICK_PERCENT_OFFSET, 0.0, 1.0)
     var click_idx := roundi(click_percent * points.size())
     line.clear_points()
     for i : int in click_idx:
         line.add_point(points[i])
-    
-    if actual_click_percent >= 1.0:
-        click_time = 0.0
-        visible = false
-        can_click = false
-        PlayerClickHelper.on_player_click(GridHelper.world_position_to_grid_position(global_position))
